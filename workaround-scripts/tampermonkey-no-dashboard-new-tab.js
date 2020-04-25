f66e87abe3cc4704bcb5292e0b88b6b3a64a2006
Alexander Thomas <doctor.lex@gmail.com>
2020-04-25 19:10:30 +0200

    New TamperMonkey script: do not open dashboard in new tab

---
+++ workaround-scripts/tampermonkey-no-dashboard-new-tab.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         Thingiverse no dashboard opening in new tab
+// @namespace    https://github.com/DrLex0/ThingiverseIssues
+// @version      0.1
+// @description  1999 called, they want their “target="_blank"” back. Let the user decide whether to open things in a new window.
+// @author       Dr. Lex
+// @include      https://www.thingiverse.com/*
+// @grant        none
+// ==/UserScript==
+
+function hack() {
+    'use strict';
+    var dashboardLinks = document.getElementsByClassName('LinkButton__linkButton--U_cBr PageHeader__dashboardLink--10X_9');
+    if(dashboardLinks) {
+        dashboardLinks[0].removeAttribute('target');
+    }
+    else {
+        console.log("Cannot hack dashboard link: element not found");
+   }
+};
+
+hack();
