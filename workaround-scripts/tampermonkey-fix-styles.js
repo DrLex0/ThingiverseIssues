624c4776c71a4096dbcfbbb966768d6e8dc560b3
Alexander Thomas <doctor.lex@gmail.com>
2023-01-14 17:34:07 +0100

    TamperMonkey scripts: fix styles again
    This time, use wildcards, which might add more robustness against the
    pointless shuffling around of style names and the randomly generated
    suffix junk that probably originates from the trendy piece of bloated
    framework they are now using to produce this pile of garbage.

--- workaround-scripts/tampermonkey-fix-styles.js
+++ workaround-scripts/tampermonkey-fix-styles.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Thingiverse fix eyesore styles
 // @namespace    https://github.com/DrLex0/ThingiverseIssues
-// @version      0.5
+// @version      0.6
 // @description  Workaround for stupid tiny pale text and other dumb styles in the new site design
 // @author       Dr. Lex
 // @match        https://www.thingiverse.com/thing:*
@@ -23,18 +23,16 @@ function GM_addStyle(css) {
 }
 
 // The horrible class names make it seem as if this is randomly generated junk that can break at any time.
+// Use the power of wildcards to combat this risk, because I have seen the class names being changed indeed.
 // This is all quick & dirty because I don't want to spend much time on something that can change at any time.
 
 function hack() {
 // Get rid of the preposterous tiny pale fonts that are just downright stupid.
 GM_addStyle ( `
-.ThingComment__commentBody--2xT45 p,
-.ThingPage__description--14TtH p,
-.ThingPage__description--14TtH div,
-.ThingPage__thingDetails--31KAR p,
-.ThingPage__thingDetails--31KAR div,
-.ThingPage__thingDetails--31KAR li,
-.ThingPage__preHistory--312bi p
+[class*="_commentBody-"] p,
+[class*="_thingDetails-"] p,
+[class*="_thingDetails-"] li,
+[class*="BlockDetails__listItem-"]
 {
     padding: 15x 20px;
     color: #000 !important;
@@ -44,28 +42,28 @@ GM_addStyle ( `
 }
 `);
 GM_addStyle ( `
-.ThingPage__blockTitle--3ZdLu {
+[class*="BlockDetails__blockTitle-"] {
     color: #000 !important;
     font-weight: bold !important;
     font-size: x-large !important;
 }
 `);
 GM_addStyle ( `
-.ThingPage__contentTitle--1sg_6 {
+[class*="_contentTitle-"] {
     color: #000 !important;
     font-weight: bold !important;
     font-size: large !important;
 }
 `);
 GM_addStyle ( `
-.ThingComment__commentTextArea--3Px7K {
+[class*="_commentTextArea-"] {
     font-size: .9em !important;
 }
 `);
 
 // Undo the stupid forcing of links onto a separate line.
 GM_addStyle ( `
-    .ThingPage__description--14TtH p a {
+    [class*="_description-"] p a {
         display: inline;
         word-break: normal;
     `
